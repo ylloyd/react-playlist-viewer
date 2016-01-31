bf31be8c4e1dd8531c6a24321d4c1c542b17d99a
kimak <mdumand@molotov.tv>
2016-01-31 19:20:47 +0100

    feat(stateless): simple first component

---
+++ web_modules/ArtistItem/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const ArtistItem = ({name ="" }) =>
+
+(<div className="artistItem">
+  {name}
+</div>)
+
+export default ArtistItem
--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import ArtistItem from "ArtistItem"
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello React !
+        <ArtistItem name="Bob Marley" />
+        <ArtistItem name="Bob Dylan" />
+        <ArtistItem name="Odezenne" />
+        <ArtistItem name="Kavinsky" />
       </div>
     );
   }
