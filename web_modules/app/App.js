57ec8c15f2271b6b42911b21f661bf9686d9d65b
kimak <mdumand@molotov.tv>
2016-02-01 22:09:05 +0100

    feat(comps): dynamics comps object array

--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
-import ArtistItem from "ArtistItem"
+import ArtistItem from "ArtistItem";
+
+const artists = [
+  {name:"Bob Marley"},
+  {name:"Bob Dylan"},
+  {name:"Odezenne"},
+  {name:"Kavinsky"}
+]
+
+const kinds = {
+  rap: {name:"Rap"},
+  rock: {name:"Rock"},
+  electro: {name:"Electro"}
+}
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <ArtistItem name="Bob Marley" />
-        <ArtistItem name="Bob Dylan" />
-        <ArtistItem name="Odezenne" />
-        <ArtistItem name="Kavinsky" />
-      </div>
-    );
+      {
+        artists &&
+        artists.map((artist, index) => {
+          return (
+              <ArtistItem key={index} name={artist.name} />
+          );
+        })
+      }
+      {
+        kinds &&
+        Object.keys(kinds).map((key, index) => {
+          return (
+              <ArtistItem key={index} name={kinds[key].name} />
+          );
+        })
+      }
+      </div>)
   }
 }
