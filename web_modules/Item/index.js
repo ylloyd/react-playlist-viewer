b4ebb883347a5829a1bec155aec656c60413c880
ylloyd <yvan@oxynum.fr>
2016-02-02 19:11:30 +0100

    feat(list): refacto component

---
+++ web_modules/Item/index.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+const Item = ({name} = "") => {
+	return <div className="item">{name}</div>
+}
+
+export default Item
--- web_modules/app/App.js
+++ web_modules/app/App.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
 
+import Item from "Item"
+import List from "List"
+
+ const artists = [
+	{name:"Rihanna"},
+	{name:"Ne-Yo"},
+	{name:"Beyoncé"},
+	{name:"Chris Brown"}
+]
+
+const kinds = {
+	rap: {name : "Rap"},
+	rock: {name : "Rock"},
+	electro: {name : "Electro"}
+}
+
+//let kindArray = Object.keys(kinds)
+//console.log(kindArray)
+
+
+// {
+// 	//(artists != null &&)
+// 	artists &&
+// 	artists.map((artist, index) => {
+// 		// return (<Item name={artist.name} />)
+// 		return this.getItem(artist, index)
+// 	})
+// }
+
+let kindArray = Object.keys(kinds)
+console.log(kindArray.length)
+
 export default class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello React !
-      </div>
-    );
-  }
+	getItem(artist, index){
+		return (<Item key={index} name={artist.name} />)
+	}
+
+	getKinds(kind, index){
+		return (<Item key={index} name={kinds[key].name} />)
+	}
+
+	render() {
+		return (
+			<div>
+		        <div>Hello React !</div>
+		        <List items={artists} />
+		        {
+		        	kinds &&
+		        	kindArray.map((key, index) => {
+		        		console.log(kinds[key].name)
+		        		return this.getItem(kinds[key], index)
+		        	})
+		        }
+			</div>
+		);
+	}
 }
